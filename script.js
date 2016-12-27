c82411765c0b21c62e16038af16b75a32b3fecdc
Jason Leong <soundslikejason@users.noreply.github.com>
2016-12-27 12:37:08 -0800

    five more modules

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   function twitchData (json, message, module, user) {
     if(json.stream) {
-      document.getElementById(module).style = "background-color: #93E251";
+      document.getElementById(module).style = "background-color: lightgreen";
       var status = json.stream.channel.status;
       var message = document.getElementById(message);
       message.innerHTML = status;
@@ -14,5 +14,25 @@ $(document).ready(function() {
 
   $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/thehypoxiaxp?callback=?", function(json){
   twitchData(json, "thehypoxiaxp-message", "thehypoxiaxp-module", "thehypoxiaxp");
+  });
+
+  $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/esl_sc2?callback=?", function(json){
+  twitchData(json, "esl_sc2-message", "esl_sc2-module", "esl_sc2");
+  });
+
+  $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/ogamingsc2?callback=?", function(json){
+  twitchData(json, "ogamingsc2-message", "ogamingsc2-module", "ogamingsc2");
+  });
+
+  $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/shannanina?callback=?", function(json){
+  twitchData(json, "shannanina-message", "shannanina-module", "shannanina");
+  });
+
+  $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/misscoookiez?callback=?", function(json){
+  twitchData(json, "misscoookiez-message", "misscoookiez-module", "misscoookiez");
+  });
+
+  $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/drezzdie?callback=?", function(json){
+  twitchData(json, "drezzdie-message", "drezzdie-module", "drezzdie");
   });
 });
